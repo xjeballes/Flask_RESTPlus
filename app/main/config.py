16d5932bd8b1dc63333570c2f4102c746a95fc9c
Xena Jane Eballes <xenajane.eballes@g.msuiit.edu.ph>
2019-03-22 23:19:17 +0800

    added test config

--- app/main/config.py
+++ app/main/config.py
@@ -10,7 +10,6 @@ class Config:
 
 
 class DevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:18345@localhost:5432/apirestplus'
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:18345@localhost:5432/apirestplus'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -19,7 +18,7 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:18345@localhost:5432/apirestplus'
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
