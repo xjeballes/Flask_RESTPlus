78af0635dbad76b9f2d0531593140aaa1b72a4d3
Xena Jane Eballes <xenajane.eballes@g.msuiit.edu.ph>
2019-03-23 04:54:46 +0800

    updated controller and manage.py

--- manage.py
+++ manage.py
@@ -3,10 +3,13 @@ import unittest
 
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
+from app import blueprint
 
 from app.main import create_app, db
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
+
+app.register_blueprint(blueprint)
 
 app.app_context().push()
 
