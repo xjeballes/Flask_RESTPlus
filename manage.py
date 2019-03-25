ab711d87f82fb64c7dacf093e1dc9373143f604f
Xena Jane Eballes <xenajane.eballes@g.msuiit.edu.ph>
2019-03-25 11:08:32 +0800

    updated manage.py, added blacklist

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ from flask_script import Manager
 from app import blueprint
 
 from app.main import create_app, db
+from app.main.model import blacklist
 
 app = create_app(os.getenv('BOILERPLATE_ENV') or 'dev')
 
